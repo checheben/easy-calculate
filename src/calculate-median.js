2f3a9f0004137e21a3754bc599f97d964b75cf30
checheben <89188446@qq.com>
2016-05-24 17:20:01 +0800

    add code

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,5 +1,20 @@
 function calculate_median(arr) {
-  //请勿改动函数名
+	var arr1 = [];
+	var returnV = 0;
+	var len = 0;
+	arr = arr.sort(function(a,b){return a-b;});
+	for(var i=0; i<arr.length; i++){
+		if(i%2){
+			arr1.push(arr[i]);
+		}
+	}
+	len = arr1.length;
+	if(len%2){
+		returnV = arr1[Math.floor(len/2)];
+	}else{
+		returnV = (arr1[len/2-1] + arr1[len/2])/2;
+	}
+	return returnV;
 }
 
 module.exports = calculate_median;
